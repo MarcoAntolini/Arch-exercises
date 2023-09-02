a0b7dc89c1e4c034d3ae3ecf821dc10fd86b9713
Marco Antolini <marco.antolini2001@gmail.com>
2023-09-02 14:50:49 +0200

    add comments

--- Elaborati/e2/main.c
+++ Elaborati/e2/main.c
@@ -16,27 +16,26 @@ void main()
 {
 	// Variabili
 	int n = 12;			 // Numero di termini da restituire
-	int successione[50]; // Vettore in cui mettere i primi n termini
-	// della successione di Fibonacci
+	int successione[50]; // Vettore in cui mettere i primi n termini della successione di Fibonacci
 
 	// Blocco Assembler
 	__asm
-	{
-		mov ecx, n
-		inc ecx
-		xor eax, eax
-		mov ebx, 1
-		xor edx, edx
-		xor esi, esi
+		{
+			mov ecx, n // Carica il valore di n nel registro ecx (contatore)
+			inc ecx // Incrementa ecx di 1 per gestire n termini
+			xor eax, eax // Azzera il registro eax (contiene il termine attuale)
+			mov ebx, 1 // Carica 1 nel registro ebx (contiene il termine precedente)
+			xor edx, edx // Azzera il registro edx (usato per lo scambio temporaneo)
+			xor esi, esi // Azzera il registro esi (usato come indice nel vettore successione)
 
-	loop_start:
-		mov successione[esi], eax
-		add esi, 4
-		mov edx, eax
-		add eax, ebx
-		mov ebx, edx
-		loop loop_start
-	}
+		loop_start:
+			mov successione[esi], eax // Memorizza il valore corrente di eax nel vettore successione
+			add esi, 4 // Incrementa l'indice esi di 4 (4 byte per int)
+			mov edx, eax // Copia il valore di eax in edx (temporaneo)
+			add eax, ebx // Aggiunge il valore di ebx a eax (genera il nuovo termine)
+			mov ebx, edx // Copia il valore originale di eax (precedente) in ebx
+			loop loop_start // Ripeti il ciclo fino a quando ecx non è zero
+		}
 
 	// Stampa su video
 	{
--- Elaborati/e3/main.c
+++ Elaborati/e3/main.c
@@ -10,42 +10,42 @@
 
  ********************************************************************************/
 
-
 #include <stdio.h>
 
-#define MAX_LEN	100
+#define MAX_LEN 100
 
 void main()
 {
 	// Input
-	unsigned char vet[] = { 0xAA, 0xFC, 0x09 };			//Array di BYTE
-	unsigned int len = sizeof(vet) / sizeof(vet[0]);	// numero di byte in vet	
+	unsigned char vet[] = {0xAA, 0xFC, 0x09};		 // Array di BYTE
+	unsigned int len = sizeof(vet) / sizeof(vet[0]); // numero di byte in vet
 	// Output
-	unsigned char res[MAX_LEN];						//Array di BYTE contenente il risultato 
+	unsigned char res[MAX_LEN]; // Array di BYTE contenente il risultato
 
 	// Blocco assembler
 	__asm
-	{
-		mov esi, 0
-		mov edi, len
-		mov ecx, len
-		
-	loop_start:
-		mov al, vet[esi]
-		xor dl, dl
-		xor ebx, ebx
-		mov ebx, 8
-	bit_loop_start:
-		rcr al, 1
-		rcl dl, 1
-		dec ebx
-		jnz bit_loop_start
+		{
+			mov esi, 0 // Inizializza l'indice sorgente (esi) a 0
+			mov edi, len // Inizializza l'indice destinazione (edi) a len
+			mov ecx, len // Inizializza il contatore (ecx) a len (numero di byte)
 
-		dec edi
-		mov res[edi], dl
-		inc esi
-		loop loop_start
-	}
+		loop_start:
+			mov al, vet[esi] // Carica il byte da vet[esi] nel registro al
+			xor dl, dl // Azzera il registro dl (sarà usato per costruire il risultato)
+			xor ebx, ebx // Azzera il registro ebx (usato come contatore per i bit)
+			mov ebx, 8 // Imposta ebx a 8 (numero di bit in un byte)
+			
+		bit_loop_start:
+			rcr al, 1 // Ruota il bit meno significativo di al nel carry (rcr = rotate through carry right)
+			rcl dl, 1 // Ruota il carry nel bit più significativo di dl (rcl = rotate through carry left)
+			dec ebx // Decrementa il contatore dei bit
+			jnz bit_loop_start // Salta a bit_loop_start se ebx non è zero (ancora bit da processare)
+
+			dec edi // Decrementa l'indice di destinazione (edi)
+			mov res[edi], dl // Memorizza il byte invertito nel vettore di risultato
+			inc esi // Incrementa l'indice sorgente (esi)
+			loop loop_start // Ripeti il ciclo fino a quando ecx non è zero
+		}
 
 	// Stampa su video
 	{
@@ -54,4 +54,3 @@ void main()
 			printf("res[%2d] = %10d (%08X)\n", i, res[i], res[i]);
 	}
 }
-
